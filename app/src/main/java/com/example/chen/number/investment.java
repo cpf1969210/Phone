d9a635c5c2c2fac30471b6f380bf081a7433e37a
cpf199210 <994185233.com>
2016-03-13 17:06:44 +0800

    2016.3.13 扫一扫

---
+++ app/src/main/java/com/example/chen/number/investment.java
@@ -0,0 +1,18 @@
+package com.example.chen.number;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.chen.phone.R;
+
+/**
+ * 投资理财
+ */
+public class investment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_investment);
+    }
+}
