150e07c03e4be05a1d85dd588739a0e213f0e5be
cpf199210 <994185233.com>
2016-03-15 15:34:21 +0800

    2016.3.15

---
+++ app/src/main/java/com/example/chen/phone/LoginActivity.java
@@ -0,0 +1,18 @@
+package com.example.chen.phone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/**
+ * 登陆界面
+ * @author CHEN
+ * 2016.3.15
+ */
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
